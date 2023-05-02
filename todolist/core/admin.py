1f1ffb6c817ea175991a91287c1ffe0543ab4564
MuriloAltoe <muriloaleme17@gmail.com>
2023-05-02 19:25:26 -0300

    model-add

---
+++ todolist/core/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import modelLista
+admin.site.register(modelLista)
+
+# Register your models here.
---
+++ todolist/core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class modelLista(models.Model):
+    tarefa = models.CharField('tarefa',max_length=70)
+    dia = models.IntegerField('dia')
+    mes = models.IntegerField('mes')
+    ano = models.IntegerField('ano')
+
+    def __str__(self):
+        return self.tarefa
+
+    class Meta:
+        verbose_name_plural = 'Lista do que fazer'
+        verbose_name = 'Lista do que fazer'
