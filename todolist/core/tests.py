5270de8a7c2a71143ff4c569e73dc44368c9acaa
unknown <f290ti@Lab05Maq32.fatecararas.com>
2023-05-03 21:29:19 -0300

    1.0

--- todolist/core/tests.py
+++ todolist/core/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+class FeriadosTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/tarefas')
 
-# Create your tests here.
+    def test_200_response(self):
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_texto(self):
+        self.assertContains(self.resp, 'tarefas')
+
+    def test_template_tarefas(self):
+        self.assertTemplateUsed(self.resp, 'index.html')
--- todolist/core/views.py
+++ todolist/core/views.py
@@ -1,3 +1,30 @@
+from datetime import datetime
+from django.http import HttpResponse
 from django.shortcuts import render
+from core.models import modelLista
+from datetime import date
 
-# Create your views here.
+def verificar_tarefas(request):
+    dia_de_hoje = date.today()
+    select_tarefas = modelLista.objects.all()
+    mensagem_para_tela = ''
+    Tarefas = {
+        'tarefas_do_dia' : mensagem_para_tela
+    }
+    all_tarefas = ''
+
+    for cursor_tarefa in select_tarefas: 
+        if (cursor_tarefa.dia == dia_de_hoje.day and cursor_tarefa.mes == dia_de_hoje.month):
+            all_tarefas = all_tarefas + ' -> ' + cursor_tarefa.tarefa     
+    
+    mensagem_para_tela = 'Hoje temos as seguintes tarefas: ' + str(all_tarefas)
+
+    if mensagem_para_tela == 'Hoje temos as seguintes tarefas: ':
+        mensagem_para_tela = 'Sem tarefas para hoje'
+    Tarefas = {
+        'tarefas_do_dia' : mensagem_para_tela
+    }        
+            
+ 
+
+    return render(request, 'index.html', Tarefas)
